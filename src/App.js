287167ded67f81b235a9f8222522454687f21c82
Wojciech <heiw17@wp.pl>
2023-09-19 10:08:20 +0200

    firstRoutercommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  Link,
+  Outlet
+} from "react-router-dom";
 
-function App() {
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<Root />} />)
+);
+const Root = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <Link to="/">
+          <p>Home</p>
+        </Link>
+        <Link to="/data">
+          <p>Data</p>
+        </Link>
+      </div>
+      <div>
+        <Outlet />
+      </div>
+    </>
   );
+};
+function App() {
+  return <div className="App">aaa</div>;
 }
 
 export default App;
